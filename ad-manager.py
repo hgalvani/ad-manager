c7cc2ba4c276717d5cd8cb74b2c859909fe12e35
hgalvani <harold.galvani@gmail.com>
2020-02-26 22:16:37 +0100

    - Switch connexion to ldaps with succed (ip address only) - Change pqssword succed - Unlock qccount succed

--- ad-manager.py
+++ ad-manager.py
@@ -7,12 +7,13 @@
 
 
 # import class and constants
-from ldap3 import Server, Connection, ALL, NTLM, Tls, SUBTREE, core, extend
+from ldap3 import Server, Connection, ALL, NTLM, Tls, SUBTREE, core, extend, MODIFY_REPLACE
 import sys
 import ssl
 
 # Variables
 AD_SERVER = '192.168.0.104'
+# AD_SERVER = 'srv-dc01.anterverse.com'
 AD_DOMAIN = 'ANTEVERSE'
 AD_BIND_USER = AD_DOMAIN + "\\ad-manager" # Use join
 AD_BIND_PWD = 'P@ssword123'
@@ -58,15 +59,16 @@ def ad_auth_ntlm(username=AD_BIND_USER, password=AD_BIND_PWD, address=AD_SERVER)
 # AD TLS connection
 # validate=ssl.CERT_REQUIRED,
 def ad_auth_ntlm_tls(username=AD_BIND_USER, password=AD_BIND_PWD, address=AD_SERVER):
-  tls_configuration = Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1)
-  s = Server(address, use_ssl=True, tls=tls_configuration, get_info=ALL)
+  # tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1)
+  # s = Server(address, use_ssl=True, tls=tls_configuration, get_info=ALL)
+  s = Server(address, port = 636, use_ssl = True, get_info=ALL)
   c = Connection(s, user=username, password=password, raise_exceptions=True, authentication=NTLM)
+  c.start_tls()
 
   # perform the Bind operation
   try:
     if not c.bind():
       print('error in bind', c.result)
-  
   except core.exceptions.LDAPBindError:
       print("Failed to bind connection")
       sys.exit(1)
@@ -81,7 +83,7 @@ def ad_auth_ntlm_tls(username=AD_BIND_USER, password=AD_BIND_PWD, address=AD_SER
   if VERBOSE:
     print ("Succesfully authenticated", c.result)
     print(c)
-    # print(s.info)    
+    print(s.info)    
 
   # Return connection
   return c     
@@ -116,13 +118,15 @@ def isExist(connexion, username):
 # You *must* use encrypted connection (i.e. LDAPS or StartSSL).
 # AD doesn’t allow changing password via unencrypted connection.
 def ad_modify_password(connexion, username, password):
-  if isExist(username):
+  # Get connection
+  c = connexion
+  if isExist(c,username):
     # Get connection
     c = connexion
-    dn = 'CN={},OU=Commerce,OU=SiteB,DC=anteverse,DC=com'.format(username)
+    dn = 'CN={},OU=SiteA,DC=anteverse,DC=com'.format(username)
 
     try:
-      r = c.extend.microsoft.modify_password(dn, old_password=None, new_password='{}').format(password)
+      r = c.extend.microsoft.modify_password(dn, old_password=None, new_password=password)
       print(r)
     except core.exceptions.LDAPNoSuchObjectResult:
       print("Failed to find user with dn {}".format(dn))
@@ -134,6 +138,8 @@ def ad_modify_password(connexion, username, password):
 # Add User
 def add_user_account(connexion, firstname, lastname, password):
   
+  lastname.upper()
+
   username = firstname + ' ' + lastname
   user_dn = 'CN={},OU=SiteA,DC=anteverse,DC=com'.format(username)
   user_object_class = ['OrganizationalPerson', 'person', 'top', 'user']
@@ -142,6 +148,7 @@ def add_user_account(connexion, firstname, lastname, password):
   'displayName': '{}'.format(username), \
   'givenName': '{}'.format(firstname), \
   'sn': '{}'.format(lastname), \
+  # 'userPrincipalName ': 'a.dupontel@anteverse.com', \
   'sAMAccountName': 'a.dupontel'}
   # attributes=['sAMAccountName', 'cn', 'givenname']
 
@@ -155,7 +162,10 @@ def add_user_account(connexion, firstname, lastname, password):
     c.add(user_dn, user_object_class, user_attributes)
   except core.exceptions.LDAPEntryAlreadyExistsResult:
     print("Entry Already Exists")
-    #sys.exit(1)  
+    #sys.exit(1)
+  except core.exceptions.LDAPNoSuchAttributeResult:
+    print("Error in attribute conversion operation")
+    #sys.exit(1)   
 
   # VERBOSE messages
   if VERBOSE:
@@ -170,11 +180,19 @@ def ad_unlock_user_account(connexion, username):
   if isExist(c,username):
     user_dn = 'CN={},OU=SiteA,DC=anteverse,DC=com'.format(username)
     try:
-      r = extend.microsoft.unlockAccount.ad_unlock_account(c, user_dn, controls=None)
-      print(r)
+      # r = extend.microsoft.unlockAccount.ad_unlock_account(c, user_dn, controls=None)
+      # print(r)
+      c.modify(user_dn, {'userAccountControl': [(MODIFY_REPLACE, ['512'])]})
+      print(c.result)
     except core.exceptions.LDAPNoSuchObjectResult:
       print("Failed to find user with dn {}".format(user_dn))
       #sys.exit(1)
+    except core.exceptions.LDAPInvalidValueError:
+      print("non valid for attribute")
+      #sys.exit(1)
+    except core.exceptions.LDAPUnwillingToPerformResult:
+      print("Unwilling to perform result : ")
+      #sys.exit(1)  
   else:
     print("{} does not exist".format(username))
 
@@ -182,6 +200,6 @@ def ad_unlock_user_account(connexion, username):
 if __name__ == "__main__":
   c = ad_auth_ntlm_tls()
   # isExist(c,'Alice DUPONT')
-  # ad_modify_password(c, 'Alice DUPONT','AliceDupond2')
-  add_user_account(c, 'Agnes', 'DUPONTEL','AliceDupond2')
-  # ad_unlock_user_account(c,'Agnes DUPONTEL')
+  # ad_modify_password(c, 'Agnes DUPONTEL', 'AliceDupond2')
+  # add_user_account(c, 'Agnes', 'DUPONTEL','AliceDupond2')
+  ad_unlock_user_account(c,'Agnes DUPONTEL')
